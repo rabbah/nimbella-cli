ded80c39ddf8da21b350045f13b5cf80b083002f
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2019-10-18 10:12:28 -0400

    Quick and dirty `nimb` replaces `wsk` using `aio`
    Issue #1

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Nimbella CONFIDENTIAL
+# ---------------------
+#
+#   2018 - present Nimbella Corp
+#   All Rights Reserved.
+#
+# NOTICE:
+#
+# All information contained herein is, and remains the property of
+# Nimbella Corp and its suppliers, if any.  The intellectual and technical
+# concepts contained herein are proprietary to Nimbella Corp and its
+# suppliers and may be covered by U.S. and Foreign Patents, patents
+# in process, and are protected by trade secret or copyright law.
+#
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Nimbella Corp.
+#
+
+# Builds the 'nim' CLI (temporarily called 'nimb')
+
+set -e
+SELFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SELFDIR
+
+npm install
+npx tsc
+npm link
