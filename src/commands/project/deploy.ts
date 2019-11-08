2f3a3c851d09fec86ad3b8a33eb4db76f7a6045b
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2019-11-08 08:51:34 -0500

    First cut of 'project watch' command
    Issue #8

--- src/commands/project/deploy.ts
+++ src/commands/project/deploy.ts
@@ -24,8 +24,8 @@ import { deployProject } from 'deployer/api'
 import { Flags, OWOptions, DeployResponse, Credentials } from 'deployer/deploy-struct'
 import { switchNamespace, fileSystemPersister } from 'deployer/login'
 
-export default class ProjectDeploy extends NimBaseCommand {
-  static description = 'Deploy a Nimbella project'
+export class ProjectDeploy extends NimBaseCommand {
+  static description = 'Deploy Nimbella projects'
 
   static flags = {
     target: flags.string({ description: 'the target namespace'}),
@@ -45,30 +45,10 @@ export default class ProjectDeploy extends NimBaseCommand {
 
   async run() {
     const {argv, flags} = this.parse(ProjectDeploy)
-    this.debug('argv', argv)
-    //this.log('flags', flags)
     const { target, env, apihost, auth, insecure, production, yarn, incremental } = flags
     const cmdFlags: Flags = { verboseBuild: flags['verbose-build'], production, incremental, env, yarn }
     this.debug('cmdFlags', cmdFlags)
-    const owOptions: OWOptions = { ignore_certs: insecure }  // No explicit undefined
-    if (apihost) {
-      owOptions.apihost = apihost
-    }
-    if (auth) {
-      owOptions.api_key = auth
-    }
-    this.debug('owOptions', owOptions)
-    // Iff a namespace switch was requested, perform it.  It might fail if there are no credentials for the target
-    let creds: Credentials|undefined = undefined
-    if (target) {
-      creds = await switchNamespace(target, owOptions.apihost, fileSystemPersister).catch((err: Error) => this.handleError(err.message, err))
-    } else if (apihost && auth) {
-      // For backward compatibility with `wsk`, we accept the absence of target when both apihost and auth are
-      // provided on the command line.  We synthesize credentials with (as yet) unknown namespace; if it later
-      // turns out that the creds conflict with a targetNamespace in the config, an error will be indicated then.
-      creds = { namespace: undefined, ow: owOptions, storageKey: undefined}
-    } /* else undefined creds; this isn't necessarily an error since the config might supply a namespace via
-        targetNamespace */
+    const { creds, owOptions } = await processCredentials(insecure, apihost, auth, target, this)
     this.debug('creds', creds)
 
     // If no projects specified, display help
@@ -79,49 +59,77 @@ export default class ProjectDeploy extends NimBaseCommand {
     // Deploy each project
     let success = true
     for (const project of argv) {
-      success = success && await this.doDeploy(project, cmdFlags, creds, owOptions)
+      success = success && await doDeploy(project, cmdFlags, creds, owOptions, this)
     }
     if (!success) {
       this.exit(1)
     }
   }
+}
 
-  async doDeploy(project: string, cmdFlags: Flags, creds: Credentials|undefined, owOptions: OWOptions): Promise<boolean> {
+// Functions also used by 'project watch'
+
+// Process credentials, possibly switch namespace
+export async function processCredentials(ignore_certs: boolean, apihost: string|undefined, auth: string|undefined, target: string|undefined,
+    logger: NimBaseCommand): Promise<{ creds: Credentials|undefined, owOptions: OWOptions }> {
+  const owOptions: OWOptions = { ignore_certs }  // No explicit undefined
+  if (apihost) {
+    owOptions.apihost = apihost
+  }
+  if (auth) {
+    owOptions.api_key = auth
+  }
+  // Iff a namespace switch was requested, perform it.  It might fail if there are no credentials for the target
+  let creds: Credentials|undefined = undefined
+  if (target) {
+    creds = await switchNamespace(target, owOptions.apihost, fileSystemPersister).catch((err: Error) => logger.handleError(err.message, err))
+  } else if (apihost && auth) {
+    // For backward compatibility with `wsk`, we accept the absence of target when both apihost and auth are
+    // provided on the command line.  We synthesize credentials with (as yet) unknown namespace; if it later
+    // turns out that the creds conflict with a targetNamespace in the config, an error will be indicated then.
+    creds = { namespace: undefined, ow: owOptions, storageKey: undefined}
+  } /* else undefined creds; this isn't necessarily an error since the config might supply a namespace via targetNamespace */
+  return { creds, owOptions }
+}
+
+// Deploy one project
+export async function doDeploy(project: string, cmdFlags: Flags, creds: Credentials|undefined, owOptions: OWOptions, logger: NimBaseCommand): Promise<boolean> {
     return deployProject(project, owOptions, creds, fileSystemPersister, cmdFlags)
-      .then((result: DeployResponse) => this.displayResult(result, project))
+      .then((result: DeployResponse) => displayResult(result, project, logger))
       .catch((err: Error) => {
-        this.displayError(err.message, err)
+        logger.displayError(err.message, err)
         return false
       })
    }
 
-  // Display the result of a successful run
-  displayResult(result: DeployResponse, project: string): boolean {
-    let namespaceClause = ""
-    if (result.namespace) {
-        namespaceClause = ` to namespace '${result.namespace}'`
-    }
-    let hostClause = ""
-    if (result.apihost) {
-        hostClause = ` on host '${result.apihost}'`
-    }
-    this.log(`\nResult of deploying project '${project}'${namespaceClause}${hostClause}`)
-    let success = true
-    if (result.successes.length == 0 && result.failures.length == 0) {
-        this.log("Nothing deployed")
-    } else {
-        for (const msg of result.successes) {
-            this.log(msg)
-        }
-        for (const err of result.failures) {
-            success = false
-            const context = (err as any)['context']
-            if (context) {
-                this.displayError(`While deploying ${context}:`, err)
-            }
-            this.displayError(err.message, err)
-          }
-    }
-    return success
+// Display the result of a successful run
+function displayResult(result: DeployResponse, project: string, logger: NimBaseCommand): boolean {
+  let namespaceClause = ""
+  if (result.namespace) {
+      namespaceClause = ` to namespace '${result.namespace}'`
   }
+  let hostClause = ""
+  if (result.apihost) {
+      hostClause = ` on host '${result.apihost}'`
+  }
+  logger.log(`\nResult of deploying project '${project}'${namespaceClause}${hostClause}`)
+  let success = true
+  if (result.successes.length == 0 && result.failures.length == 0) {
+      logger.log("Nothing deployed")
+  } else {
+      for (const msg of result.successes) {
+          logger.log(msg)
+      }
+      for (const err of result.failures) {
+          success = false
+          const context = (err as any)['context']
+          if (context) {
+              logger.displayError(`While deploying ${context}:`, err)
+          }
+          logger.displayError(err.message, err)
+        }
+  }
+  return success
 }
+
+export default ProjectDeploy
---
+++ src/commands/project/watch.ts
@@ -0,0 +1,105 @@
+/*
+ * Nimbella CONFIDENTIAL
+ * ---------------------
+ *
+ *   2018 - present Nimbella Corp
+ *   All Rights Reserved.
+ *
+ * NOTICE:
+ *
+ * All information contained herein is, and remains the property of
+ * Nimbella Corp and its suppliers, if any.  The intellectual and technical
+ * concepts contained herein are proprietary to Nimbella Corp and its
+ * suppliers and may be covered by U.S. and Foreign Patents, patents
+ * in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Nimbella Corp.
+ */
+
+import { NimBaseCommand } from '../../NimBaseCommand'
+import { ProjectDeploy, processCredentials, doDeploy } from './deploy'
+import { Flags, Credentials, OWOptions } from 'deployer/deploy-struct'
+import * as fs from 'fs'
+import * as path from 'path'
+
+export default class ProjectWatch extends NimBaseCommand {
+  static description = 'Watch Nimbella projects, deploying incrementally on change'
+
+  static flags = {
+    target: ProjectDeploy.flags.target,
+    env: ProjectDeploy.flags.env,
+    apihost: ProjectDeploy.flags.env,
+    auth: ProjectDeploy.flags.auth,
+    insecure: ProjectDeploy.flags.insecure,
+    'verbose-build': ProjectDeploy.flags['verbose-build'],
+    yarn: ProjectDeploy.flags.yarn,
+    ...NimBaseCommand.flags
+  }
+
+  static args = ProjectDeploy.args
+  static strict = false
+
+  async run() {
+    const {argv, flags} = this.parse(ProjectWatch)
+    const { target, env, apihost, auth, insecure, yarn } = flags
+    const cmdFlags: Flags = { verboseBuild: flags['verbose-build'], production: false, incremental: true, env, yarn }
+    this.debug('cmdFlags', cmdFlags)
+    const { creds, owOptions } = await processCredentials(insecure, apihost, auth, target, this)
+        .catch(err => this.handleError(err.message, err))
+    argv.forEach(project => watch(project, cmdFlags, creds, owOptions, this))
+  }
+}
+
+// Validate a project and start watching it if it actually looks like a project
+// Note: we can't just watch the project directory but have to watch what's in it, skipping the .nimbella directory.
+// Otherwise, we go into an infinite loop because every deploy modifies that directory.
+// As it is, mini-builds usually do modifications and so we might get two deploys (the second hopefully vacuous) after
+// a modification that causes a build.
+function watch(project: string, cmdFlags: Flags, creds: Credentials|undefined, owOptions: OWOptions, logger: NimBaseCommand) {
+    const msg = validateProject(project)
+    if (msg) {
+        logger.handleError(msg, new Error(msg))
+    }
+    logger.log(`Watching ${project}`)
+    try {
+        fs.readdirSync(project).forEach(member => {
+            if (member !== '.nimbella') {
+                fs.watch(path.join(project, member), { recursive: true, persistent: true},
+                    () => doDeploy(project, cmdFlags, creds, owOptions, logger))
+            }
+        })
+    } catch (err) {
+        logger.handleError(err.message, err)
+    }
+}
+
+// Validate a project argument to ensure that it denotes an actual directory that "looks like a project".
+// Returns an error message when there is a problem, undefined
+function validateProject(project: string): string|undefined {
+    if (!fs.existsSync(project)) {
+        return `${project} does not exist`
+    }
+    const stat = fs.lstatSync(project)
+    if (!stat.isDirectory()) {
+        return `${project} is not a directory`
+    }
+    if (isTypicalProject(project, 'project.yml', true) || isTypicalProject(project, 'packages', false)
+            || isTypicalProject(project, 'web', false)) {
+        return undefined
+    }
+    return `${project} is a directory but it doesn't appear to contain a project`
+}
+
+// Check for typical things found in a project (part of validating that a directory is a project)
+function isTypicalProject(project: string, item: string, shouldBeFile: boolean): boolean {
+    item = path.join(project, item)
+    if (fs.existsSync(item)) {
+        const stat = fs.lstatSync(item)
+        if (shouldBeFile && stat.isFile() || !shouldBeFile && stat.isDirectory()) {
+            return true
+        }
+    }
+    return false
+}
