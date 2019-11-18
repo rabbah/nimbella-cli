b8200a66eb46bf59fca8de4d2d5c1381084440fc
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2019-11-18 11:25:54 -0500

    Template for linux install (not yet incorporated)
    Issue #39

---
+++ nim-install-linux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script installs current linux self-contained tarball on a target system
+# It should be run under sudo as in
+#   curl https://<URL-of-this-script> | sudo bash
+
+# URL of the tarball to install (generated during build)
+URL=https://apigcp.nimbella.io/downloads/nim/nim-%V%/nim-%V%-linux-x64.tar.gz
+
+# Download and unpack the tarball
+set -e
+curl -s $URL > /tmp/nim-install.tgz
+tar xzf /tmp/nim-install.tgz
+
+# Swap in the new version
+rm -fr /usr/local/lib/nimbella-cli
+mv /tmp/nim /usr/local/lib/nimbella-cli
+
+# Swap in the new symlink
+rm /usr/local/bin/nim
+ln -s /usr/local/lib/nimbella-cli/bin/nim /usr/local/bin
