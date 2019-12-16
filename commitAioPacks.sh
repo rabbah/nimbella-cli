ea3b8c0232841dd26a4e5d2bf6d5d0b5e9df98a4
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2019-12-16 10:57:34 -0500

    WIP for adding --dummy-oclif flag to aio packs
    Issue #75

--- commitAioPacks.sh
+++ commitAioPacks.sh
@@ -20,6 +20,15 @@
 #
 #
 # This script runs makeAioPacks.sh surrounded by checks for consistency and commits the result
+# With the --dummy-oclif flag it uses the dummy oclif as aio's @oclif/command dependency and
+#  commits the result under a special name.
+
+if [ "$1" == "--dummy-oclif" ] && [ -n "$2" ]; then
+		DUMMY_OCLIF="$2"
+elif [ -n "$1" ]; then
+		echo "Incorrect argument(s)"
+		exit 1
+fi
 
 set -e
 SELFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -30,38 +39,45 @@ MAINDIR=$SELFDIR/../main
 pushd ../aio-cli-plugin-runtime
 DIRTY=$(git status --porcelain)
 BR=$(git symbolic-ref HEAD --short)
-popd
 if [ -n "$DIRTY" ]; then
 		echo "aio-cli-plugin-runtime has uncommitted changes"
+		popd
 		exit 1
 fi
 if [ "$BR" != "dev" ]; then
 		echo "aio-cli-plugin-runtime is not on the 'dev' branch"
+		popd
 		exit 1
 fi
 
+# If requested, temp-edit the package.json for the aio pack build to use dummy oclif
+if [ -n "$DUMMY_OCLIF" ]; then
+		URL="https://nimaio-apigcp.nimbella.io/$DUMMY_OCLIF"
+		jq -r '.dependencies."@oclif/command" = "'$URL'"' < package.json > tmp.json
+		mv tmp.json package.json
+fi
+
 # Run the aio pack build
+popd
 ./makeAioPacks.sh
 
-# Record, then retrieve, the aio hash
-./aioUpToDate.sh record
-HASH=$(cat aio.hash)
-TARBALL_NAME="adobe-aio-cli-plugin-runtime-$HASH.tgz"
+# Clean up from modified package.json
+pushd ../aio-cli-plugin-runtime
+git checkout package.json
+popd
 
-# Copy the tarball into the aiodeploy deployer project
-rm -fr aiodeploy/web
-mkdir -p aiodeploy/web
-cp adobe-aio-cli-plugin-runtime.tgz aiodeploy/web/$TARBALL_NAME
-
-# Perform the deployment to make the tarball visible by https
-PROJECT=nimdev
-if [ -f $MAINDIR/config/nimconfig.json ]; then
-	PROJECT=$(jq -r .current < "$MAINDIR/config/nimconfig.json")
+# When not making the dummy version, record then retrieve the aio hash and generate tarball name using it
+# For the dummy version, the aio that includes the dummy oclif has a name generated from the dummy oclif name
+if [ -z "$DUMMY_OCLIF" ]; then
+		./aioUpToDate.sh record
+		HASH=$(cat aio.hash)
+		TARBALL_NAME="adobe-aio-cli-plugin-runtime-$HASH.tgz"
+else
+		TARBALL_NAME="aio-with-$DUMMY_OCLIF"
 fi
-nimadmin project set nimgcp
-echo yes | nimadmin user set nimbella nimaio
-nimadmin project set $PROJECT
-nim project deploy aiodeploy
+
+# Upload the tarball
+gsutil cp adobe-aio-cli-plugin-runtime.tgz gs://nimaio-apigcp.nimbella.io/$TARBALL_NAME
 
 # Edit package.json so that the correct dependency is declared there
 URL="https://nimaio-apigcp.nimbella.io/$TARBALL_NAME"
