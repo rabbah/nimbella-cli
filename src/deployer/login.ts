a627b50de4953a27136f6ea4f8fbf2cd4439fab4
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2020-02-13 06:22:11 -0500

    Fix spelling in comment

--- src/deployer/login.ts
+++ src/deployer/login.ts
@@ -190,10 +190,10 @@ export async function forgetNamespace(namespace: string, apihost: string|undefin
 }
 
 // Switch the active namespace in the credential store.  The namespace argument is required.
-// All occurances of the namespace across all API hosts are collected.
+// All occurences of the namespace across all API hosts are collected.
 // If there is an explicit 'apihost' argument this collection must include an entry with that API host
 // Otherwise,
-//   - if there is just one occurance, the switch is to that namespace on that API host
+//   - if there is just one occurence, the switch is to that namespace on that API host
 //   - otherwise, no switch occurs and the thrown Error either states that no credentials exist for that namespace
 //     or that the --apihost flag is required to indicate which one is intended
 export async function switchNamespace(namespace: string, apihost: string|undefined, persister: Persister): Promise<Credentials> {
