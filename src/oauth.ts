576898817083f064f6c2c8a5dc9bada832495f5f
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2020-03-17 18:34:44 -0400

    Make oauth module safe to include in web browser
    This does not mean it's functionality works in a web browser.   A
    follow-on step will be needed for that.  Issues #50 and #139

--- src/oauth.ts
+++ src/oauth.ts
@@ -19,9 +19,7 @@
  */
 
 import  * as querystring from 'querystring'
-import { createServer} from 'http'
-import * as getPort from 'get-port'
-import { NimLogger } from './NimBaseCommand'
+import { NimLogger, inBrowser } from './NimBaseCommand'
 
 // These types duplicate declarations in main/deployable/login, except that Credentials is renamed to FullCredentials
 // to avoid confusing it with the Credentials type used throught nim.
@@ -64,66 +62,67 @@ function providerFromResponse(response: OAuthResponse): string {
 
 // Do an interactive token flow, either to establish an Nimella account or to add a github account
 export async function doOAuthFlow(logger: NimLogger, githubOnly: boolean): Promise<OAuthResponse> {
-  const port = await getPort({ port: 3000 })
-  let deferredResolve: (response: OAuthResponse) => void
-  let deferredReject
-  const deferredPromise = new Promise<OAuthResponse>(function(resolve, reject) {
-    deferredResolve = resolve
-    deferredReject = reject
-  })
-
-  const server = createServer(function(req, res) {
-    const parameters = querystring.parse(req.url.slice(req.url.indexOf('?') + 1))
-    if (parameters.token) {
-      let response: OAuthResponse
-      try {
-        const buffer = Buffer.from(parameters.token as string, 'base64')
-        response = JSON.parse(buffer.toString('ascii'))
-        deferredResolve(response)
-      } catch (e) {
-        deferredReject(e)
-      }
-
-      res.end(
-        "<html><head><style>html{font-family:sans-serif;background:#0e1e25}body{overflow:hidden;position:relative;display:flex;flex-direction:column;align-items:center;justify-content:center;height:100vh;width:100vw;}h3{margin:0}.card{position:relative;display:flex;flex-direction:column;width:75%;max-width:364px;padding:24px;background:white;color:rgb(14,30,37);border-radius:8px;box-shadow:0 2px 4px 0 rgba(14,30,37,.16);}</style></head>" +
-          "<body><div class=card><h3>Logged In</h3><p>You're now logged into Nimbella CLI with your " +
-          providerFromResponse(response) +
-          ' credentials. Please close this window.</p></div>'
-      )
-      server.close()
-      return
-    }
-    res.end('BAD PARAMETERS')
-    server.close()
-    deferredReject(new Error('Got invalid parameters for CLI login'))
-  })
-
-  await new Promise(function(resolve, reject) {
-    server.on('error', reject)
-    server.listen(port, resolve)
-  })
-
-  const webUI = 'https://preview-apigcp.nimbella.io/api'
-
-  const url =
-    webUI +
-    '/user/login?' +
-    querystring.stringify({
-      redirect: true,
-      port: port,
-      provider: githubOnly ? 'github' : undefined
+  if (!inBrowser) {
+    const createServer = require('http').createServer
+    const getPort = require('get-port')
+    const port = await getPort({ port: 3000 })
+    let deferredResolve: (response: OAuthResponse) => void
+    let deferredReject
+    const deferredPromise = new Promise<OAuthResponse>(function(resolve, reject) {
+      deferredResolve = resolve
+      deferredReject = reject
     })
 
-  try {
-    // Using require here instead of import at module scope because open will break in a browser; if we put it at module scope
-    // it may keep workbench from running at all, depending on how webpack groups things.  As it is, the code will fail in a
-    // browser.   TODO get it working in a browser
-    await require('open')(url)
-  } catch (err) {
-    logger.handleError('Nimbella CLI could not open the browser for you.' +
-      ' Please visit this URL in a browser on this device: ' + url,
-      err)
-  }
+    const server = createServer(function(req, res) {
+      const parameters = querystring.parse(req.url.slice(req.url.indexOf('?') + 1))
+      if (parameters.token) {
+        let response: OAuthResponse
+        try {
+          const buffer = Buffer.from(parameters.token as string, 'base64')
+          response = JSON.parse(buffer.toString('ascii'))
+          deferredResolve(response)
+        } catch (e) {
+          deferredReject(e)
+        }
 
-  return await deferredPromise
+        res.end(
+          "<html><head><style>html{font-family:sans-serif;background:#0e1e25}body{overflow:hidden;position:relative;display:flex;flex-direction:column;align-items:center;justify-content:center;height:100vh;width:100vw;}h3{margin:0}.card{position:relative;display:flex;flex-direction:column;width:75%;max-width:364px;padding:24px;background:white;color:rgb(14,30,37);border-radius:8px;box-shadow:0 2px 4px 0 rgba(14,30,37,.16);}</style></head>" +
+            "<body><div class=card><h3>Logged In</h3><p>You're now logged into Nimbella CLI with your " +
+            providerFromResponse(response) +
+            ' credentials. Please close this window.</p></div>'
+        )
+        server.close()
+        return
+      }
+      res.end('BAD PARAMETERS')
+      server.close()
+      deferredReject(new Error('Got invalid parameters for CLI login'))
+    })
+
+    await new Promise(function(resolve, reject) {
+      server.on('error', reject)
+      server.listen(port, resolve)
+    })
+
+    const webUI = 'https://preview-apigcp.nimbella.io/api'
+
+    const url =
+      webUI +
+      '/user/login?' +
+      querystring.stringify({
+        redirect: true,
+        port: port,
+        provider: githubOnly ? 'github' : undefined
+      })
+
+    try {
+      await require('open')(url)
+    } catch (err) {
+      logger.handleError('Nimbella CLI could not open the browser for you.' +
+        ' Please visit this URL in a browser on this device: ' + url,
+        err)
+    }
+
+    return await deferredPromise
+  }
 }
