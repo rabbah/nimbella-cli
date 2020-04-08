ac37829655b7ae2fcff9893e360c9050ee30b035
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2020-04-08 15:33:46 -0400

    Action invoke default becomes --full in workbench
    Issue #167

--- src/commands/action/invoke.ts
+++ src/commands/action/invoke.ts
@@ -18,17 +18,32 @@
  * from Nimbella Corp.
  */
 
-import { NimBaseCommand, NimLogger } from '../../NimBaseCommand'
+import { NimBaseCommand, NimLogger, inBrowser } from '../../NimBaseCommand'
 import { RuntimeBaseCommand } from '@adobe/aio-cli-plugin-runtime'
 const AioCommand: typeof RuntimeBaseCommand = require('@adobe/aio-cli-plugin-runtime/src/commands/runtime/action/invoke')
 
 export default class ActionInvoke extends NimBaseCommand {
   async runCommand(rawArgv: string[], argv: string[], args: any, flags: any, logger: NimLogger) {
+    // Ensure correct results in the workbench
+    if (inBrowser) {
+      this.debug('flags: %O', flags)
+      // Impose oclif convention that boolean flags are really boolean, since action invoke logic depends on this.
+      // Perhaps this should be done earlier since it represents a difference between kui and oclif.  Kui also
+      // handles the '--no-' prefix differently: --no-wait will set --wait to false, not --no-wait to true.  On the
+      // other hand, the abbreviation -n will indeed set --no-wait to true.
+      flags.result == !!flags.result
+      flags['no-wait'] = flags['no-wait'] || flags.wait === false
+      // Also impose a different default (--full, rather than --result).
+      flags.full = !flags.result && !flags['no-wait']
+      this.debug('adjusted flags: %O', flags)
+    }
     await this.runAio(rawArgv, argv, args, flags, logger, AioCommand)
   }
 
   static args = AioCommand.args
 
+  // The template for parsing the flags is not changed for the browser because it is only used by oclif parsing
+  // The browser inverts the flags by a special case in the usage model generator.
   static flags = AioCommand.flags
 
   static description = AioCommand.description
